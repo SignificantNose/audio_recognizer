54b411d89da184d907aeb107433a75b6d41b34d6
significantnose <yanucevich.d@gmail.com>
2024-10-09 10:10:18 +0300

    Gateway microservice containerized (still need to change microservice addresses)

--- RecognizerGateway/Program.cs
+++ RecognizerGateway/Program.cs
@@ -14,11 +14,11 @@ builder.Services.Configure<MicroserviceAddresses>(builder.Configuration.GetSecti
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
